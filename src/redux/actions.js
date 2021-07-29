129f94f50b8afa337fb691b77dfb5852207ba8f2
Sean Reid <sean@torchcodelab.com>
2021-07-29 14:26:02 -0400

    Refactored to remove the actionTypes file

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,16 +1,18 @@
-import { ACTION_EAT, ACTION_NAP, ACTION_PLAY } from './actionTypes';
+export const ACTION_EAT = 'eating';
+export const ACTION_NAP = 'napping';
+export const ACTION_PLAY = 'play';
 
 export const actionEat = () => {
     return {
-        type: ACTION_EAT
-    }
-}
+        type: ACTION_EAT,
+    };
+};
 
 export const actionNap = () => {
     return {
-        type: ACTION_NAP
-    }
-}
+        type: ACTION_NAP,
+    };
+};
 
 export const actionPlay = () => {
     return {
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,18 +1,18 @@
-import { ACTION_EAT, ACTION_NAP, ACTION_PLAY } from './actionTypes';
+import { ACTION_EAT, ACTION_NAP, ACTION_PLAY } from './actions';
 
 export const cat = (state, action) => {
     switch (action.type) {
         case ACTION_EAT:
             return {
-                activity: 'eating',
+                activity: ACTION_EAT,
             };
         case ACTION_PLAY:
             return {
-                activity: 'playing',
+                activity: ACTION_PLAY,
             };
         case ACTION_NAP:
             return {
-                activity: 'napping',
+                activity: ACTION_NAP,
             };
         default:
             return state;
